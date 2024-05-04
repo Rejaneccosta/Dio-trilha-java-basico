cfaa1fb7e9098419a315a47f0a68a30f564b4591
Rejaneccosta <168275962+Rejaneccosta@users.noreply.github.com>
2024-05-04 20:11:37 -0300

    pratica

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,10 +2,11 @@ public class TiposVariaveis {
 
 
 public static void main(String[] args) {
-
     int numero =1;
+
     numero = 10;
-    sytem.out.print (numero);
+
+    System.out.print(numero);
 
 }
 }
